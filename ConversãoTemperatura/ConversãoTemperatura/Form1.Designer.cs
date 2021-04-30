1bac01b634debd2fed7264e51dab1f4d9e1a4860
AndreyMouraGouvea <andreygouveia.05@gmail.com>
2021-04-30 11:56:24 -0300

    ConvertToFahrenheit and Kelvin

---
+++ ConversãoTemperatura/ConversãoTemperatura/Form1.Designer.cs
@@ -0,0 +1,128 @@
+﻿
+namespace ConversãoTemperatura
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.fah = new System.Windows.Forms.Button();
+            this.kel = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(75, 63);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(59, 21);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Celsius";
+            // 
+            // fah
+            // 
+            this.fah.BackColor = System.Drawing.Color.Pink;
+            this.fah.Font = new System.Drawing.Font("Segoe UI", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.fah.Location = new System.Drawing.Point(18, 163);
+            this.fah.Name = "fah";
+            this.fah.Size = new System.Drawing.Size(116, 35);
+            this.fah.TabIndex = 1;
+            this.fah.Text = "Fahrenheit";
+            this.fah.UseVisualStyleBackColor = false;
+            this.fah.Click += new System.EventHandler(this.fah_Click);
+            // 
+            // kel
+            // 
+            this.kel.BackColor = System.Drawing.Color.Orange;
+            this.kel.Font = new System.Drawing.Font("Segoe UI", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.kel.Location = new System.Drawing.Point(188, 163);
+            this.kel.Name = "kel";
+            this.kel.Size = new System.Drawing.Size(125, 35);
+            this.kel.TabIndex = 2;
+            this.kel.Text = "Kelvin";
+            this.kel.UseVisualStyleBackColor = false;
+            this.kel.Click += new System.EventHandler(this.kel_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(163, 65);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 23);
+            this.textBox1.TabIndex = 3;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(163, 107);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.Size = new System.Drawing.Size(100, 23);
+            this.textBox2.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(75, 105);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(79, 21);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Resultado";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(325, 260);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.kel);
+            this.Controls.Add(this.fah);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button fah;
+        private System.Windows.Forms.Button kel;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ ConversãoTemperatura/ConversãoTemperatura/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ConversãoTemperatura
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void fah_Click(object sender, EventArgs e)
+        {
+            double f = Convert.ToDouble(textBox1.Text);
+            textBox2.Text = Convert.ToString((f * 1.8 + 32));
+            MessageBox.Show("Fahrenheit: "+ textBox2);
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void kel_Click(object sender, EventArgs e)
+        {
+            double k = Convert.ToDouble(textBox1.Text);
+            textBox2.Text = Convert.ToString((k + 273.15));
+            MessageBox.Show("Kelvin: " + textBox2);
+        }
+    }
+}
